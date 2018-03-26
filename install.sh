449e2889769f4b29e2e662d474e0e29e54488ebc
Mariano Pancaldi <31867055+banzekin@users.noreply.github.com>
2018-03-26 08:44:08 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 #Name        = "my_aliases"
-#Description = "Bash alias compilation"
-#Version     = "1.0.0"
-#ReleaseDate = "2018-03-22"
+#Description = "Install script"
+#Version     = "1.0.1"
+#ReleaseDate = "2018-03-26"
 
-clear
 mkdir ~/.my_aliases_data
 cp .my_aliases ~/
 cp .networking ~/.my_aliases_data
@@ -14,13 +13,16 @@ cp .media ~/.my_aliases_data
 cp .scantools_alias ~/.my_aliases_data
 cp .banner ~/.my_aliases_data
 
-cat << EOF >> ~/.bashrc
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
-# Added by my_aliases bash script #
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
+clear
+read -p "Do you wish to add the banner login? [y/n] " install
+echo ""
+    if [[ "$install" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
+clear
+echo '
+ #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
+ # Added by my_aliases bash script #
+ #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
 
-# The banner needs the package "lolcat" for working
-# If you don't want to use it comment the line
 cat ~/.my_aliases_data/.banner | lolcat && ddate && echo ""
 
 if [ -f ~/.my_aliases ]; then
@@ -41,8 +43,35 @@ fi
 
 if [ -f ~/.my_aliases_data/.media ]; then
     . ~/.my_aliases_data/.media
+fi' >> ~/.bashrc
+    else
+clear
+echo '
+ #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
+ # Added by my_aliases bash script #
+ #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
+
+if [ -f ~/.my_aliases ]; then
+    . ~/.my_aliases
 fi
-EOF
+
+if [ -f ~/.my_aliases_data/.networking ]; then
+    . ~/.my_aliases_data/.networking
+fi
+
+if [ -f ~/.my_aliases_data/.fw_rules ]; then
+    . ~/.my_aliases_data/.fw_rules
+fi
+
+if [ -f ~/.my_aliases_data/.scantools_alias ]; then
+    . ~/.my_aliases_data/.scantools_alias
+fi
+
+if [ -f ~/.my_aliases_data/.media ]; then
+    . ~/.my_aliases_data/.media
+fi' >> ~/.bashrc
+rm -rf ~/.my_aliases_data/.banner
+fi
 
 #updating aliases
 source /home/$USER/.bashrc
